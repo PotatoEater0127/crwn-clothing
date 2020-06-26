ced9d83d3e9a27b2e9fd7268a4d597a062197e27
Henry Han <potatoeater0127@gmail.com>
2020-06-26 16:12:06 +0800

    (feat) implement firebase util for sign-in and sign-up with google/email

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,26 @@ const config = {
   appId: "1:811526194600:web:2f090b4b5f76c611ed899d",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({ displayName, email, createdAt, ...additionalData });
+    } catch (err) {
+      console.log("error creating user", err.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
